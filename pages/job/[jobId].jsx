8a35f87a92b80e2a72d8b902ac6874c0193dd2e6
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2022-12-22 15:38:45 -0800

    Start home and job pages

---
+++ pages/job/[jobId].jsx
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+function Job(props) {
+  // TODO: Display Job title, description, created Date, Lab name
+  return (
+    <ul>
+      <li>{props.job.title}</li>
+      <li></li>
+    </ul>
+  );
+}
+
+export async function getStaticProps(context) {
+  // TODO: change prisma query using findUnique and context. Only select needed columns
+  const job = await prisma.job.findFirst({ include: { posters: true, lab: true } });
+  // console.log({ job });
+  return {
+    props: { job: { ...job, created: JSON.parse(JSON.stringify(job.created)) } },
+  };
+}
+
+export async function getStaticPaths() {
+  // TODO: change prisma query. Only select needed rows and column(s)
+  const jobs = await prisma.job.findMany();
+
+  const paths = jobs.map((job) => ({
+    params: { jobId: job.id.toString(10) },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export default Job;
