4348e61e410d8ca1db4fed64052b0c5b59d74aee
paul-serafimescu <paulserafimescu@gmail.com>
2023-02-06 00:17:52 -0800

    add seeding and development postgres build

---
+++ prisma/seed.js
@@ -0,0 +1,41 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const lab = await prisma.lab.upsert({
+    where: { slug: '12345678' },
+    create: {
+      name: 'my lab',
+      slug: '12345678',
+      description: 'lab description',
+    },
+    update: {},
+  });
+
+  const job = await prisma.job.upsert({
+    where: { id: 1 },
+    create: {
+      closingDate: new Date(),
+      closed: false,
+      title: 'test job',
+      description: 'test job description',
+      labId: lab.id,
+      paid: true,
+      duration: 'Quarterly',
+      weeklyHours: 10,
+      credit: true,
+    },
+    update: {},
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
