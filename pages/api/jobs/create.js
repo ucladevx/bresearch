3122337c0fc20707cab18af910f1502ae6881852
Paul Serafimescu <56523261+paul-serafimescu@users.noreply.github.com>
2023-03-02 09:01:30 -0800

    Add Job creation endpoint (#18)
    * Add Student and Job fields
    
    * add seeding and development postgres build
    
    * database setup documentation
    
    * remove migrations
    
    * Update job duration
    
    * Fix serializing Date error
    
    * add migrations to db setup instructions
    
    * initial migrations
    
    * merge changes from main
    
    * path alias and class based routes
    
    * job creation endpoint
    
    * update route abstract class and job endpoint
    
    * input validation and handler security fix
    
    * global prisma client
    
    ---------
    
    Co-authored-by: Joseph Yu <39754176+joseph082@users.noreply.github.com>

---
+++ pages/api/jobs/create.js
@@ -0,0 +1,106 @@
+import { Prisma } from 'prisma/prisma-client';
+
+import ApiRoute from '@lib/ApiRoute';
+import BaseError, { MaximumSizeExceededError } from '@lib/Errors';
+
+// temporary session mock
+const getServerSession = async (..._) => ({
+  user: {
+    name: 'John Doe',
+    email: 'johndoe@g.ucla.edu',
+    image: null,
+  },
+  expires: new Date(new Date().getDate() + 1).toISOString(),
+});
+
+/* example POST request body
+{
+    "closingDate": "2023-02-24T01:48:47.344Z",
+    "title": "my first job post",
+    "description": "description for my first job post",
+    "labId": "d6ded724-b13b-47e1-b8e1-64ecc8808948",
+    "paid": false,
+    "duration": "QUARTERLY",
+    "departments": ["ENGINEERING"],
+    "weeklyHours": 30,
+    "credit": true
+}
+*/
+
+class JobCreationRoute extends ApiRoute {
+  /**
+   * job creation endpoint
+   * @param {import('next').NextApiRequest} req see above example request body
+   * @param {import('next').NextApiResponse} res
+   * @param {import('prisma/prisma-client').PrismaClient} prisma
+   * @returns
+   */
+  async post(req, res, prisma) {
+    const maxDescriptionLength = 15_000;
+    const maxWeeklyHours = 24 * 7;
+    const session = await getServerSession(req, res);
+    const {
+      closingDate,
+      title,
+      description,
+      labId,
+      paid,
+      duration,
+      departments,
+      weeklyHours,
+      credit,
+    } = req.body;
+
+    try {
+      if (description.length > maxDescriptionLength)
+        throw new MaximumSizeExceededError(
+          `description field exceeds ${maxDescriptionLength} character limit`
+        );
+      if (weeklyHours > maxWeeklyHours)
+        throw new MaximumSizeExceededError(
+          `weekly hours field exceeds ${maxWeeklyHours} maximum hours per week`
+        );
+      const closeDate = new Date(closingDate);
+
+      const result = await prisma.job.create({
+        data: {
+          closingDate: closeDate,
+          closed: closeDate < new Date(),
+          title,
+          description,
+          lab: { connect: { id: labId } },
+          posters: {
+            connect: [{ email: session.user.email }],
+          },
+          paid,
+          duration,
+          departments: [...new Set(departments)],
+          weeklyHours,
+          credit,
+        },
+      });
+
+      res.json(result);
+    } catch (e) {
+      // check for Node.js errors (data integrity, etc)
+      switch (e.name) {
+        case 'RangeError':
+          return res.status(400).json({ message: 'Invalid closing date' });
+        default: // exceptions
+          if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            res.status(500).json({ message: e.meta });
+          } else if (e instanceof Prisma.PrismaClientValidationError) {
+            res.status(400).json({ message: 'Invalid data format' });
+          } else if (e instanceof BaseError) {
+            res.status(400).json({ message: e.message });
+          } else {
+            res.status(500);
+          }
+      }
+    } finally {
+      res.end();
+    }
+  }
+}
+
+export default new JobCreationRoute().as_handler();
