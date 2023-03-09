7cf9216d8110cd51f423cf262b9378437a3e2c28
paul-serafimescu <paulserafimescu@gmail.com>
2023-03-08 22:36:30 -0800

    add validation + route auth

--- pages/api/jobs/create.js
+++ pages/api/jobs/create.js
@@ -1,20 +1,19 @@
 import { Prisma } from 'prisma/prisma-client';
 
+import { JobCreationValidator, isValidationError } from '@lib/validators';
 import ApiRoute from '@lib/ApiRoute';
-import BaseError, { MaximumSizeExceededError } from '@lib/Errors';
-
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]';
 
 /* example POST request body
 {
-    "closingDate": "2023-02-24T01:48:47.344Z",
+    "closingDate": "2023-03-18T23:28:19.179Z",
     "title": "my first job post",
     "description": "description for my first job post",
     "labId": "d6ded724-b13b-47e1-b8e1-64ecc8808948",
     "paid": false,
     "duration": "QUARTERLY",
-    "departments": ["ENGINEERING"],
+    "departments": [
+        "ENGINEERING"
+    ],
     "weeklyHours": 30,
     "credit": true
 }
@@ -23,40 +22,31 @@ import { authOptions } from '../auth/[...nextauth]';
 class JobCreationRoute extends ApiRoute {
   /**
    * job creation endpoint
-   * @param {import('next').NextApiRequest} req see above example request body
+   * @param {import('next').NextApiRequest & { session: import('next-auth').Session?}} req see above example request body
    * @param {import('next').NextApiResponse} res
    * @param {import('prisma/prisma-client').PrismaClient} prisma
    * @returns
    */
   async post(req, res, prisma) {
-    const maxDescriptionLength = 15_000;
-    const maxWeeklyHours = 24 * 7;
-    const session = await getServerSession(req, res, authOptions);
-    if (!session) {
-      res.status(401).json({ message: 'Not logged in' });
-      return;
-    }
-    const {
-      closingDate,
-      title,
-      description,
-      labId,
-      paid,
-      duration,
-      departments,
-      weeklyHours,
-      credit,
-    } = req.body;
-
     try {
-      if (description.length > maxDescriptionLength)
-        throw new MaximumSizeExceededError(
-          `description field exceeds ${maxDescriptionLength} character limit`
-        );
-      if (weeklyHours > maxWeeklyHours)
-        throw new MaximumSizeExceededError(
-          `weekly hours field exceeds ${maxWeeklyHours} maximum hours per week`
-        );
+      const { error, value } = JobCreationValidator.validate(req.body);
+
+      if (error) {
+        throw error;
+      }
+
+      const {
+        closingDate,
+        title,
+        description,
+        labId,
+        paid,
+        duration,
+        departments,
+        weeklyHours,
+        credit,
+      } = value;
+
       const closeDate = new Date(closingDate);
 
       const result = await prisma.job.create({
@@ -67,32 +57,28 @@ class JobCreationRoute extends ApiRoute {
           description,
           lab: { connect: { id: labId } },
           posters: {
-            connect: [{ email: session.user.email }],
+            connect: [{ email: req.session.user.email }],
           },
           paid,
           duration,
-          departments: [...new Set(departments)],
+          departments,
           weeklyHours,
           credit,
         },
       });
 
-      res.json(result);
+      res.status(200).json(result);
     } catch (e) {
       // check for Node.js errors (data integrity, etc)
-      switch (e.name) {
-        case 'RangeError':
-          return res.status(400).json({ message: 'Invalid closing date' });
-        default: // exceptions
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            res.status(500).json({ message: e.meta });
-          } else if (e instanceof Prisma.PrismaClientValidationError) {
-            res.status(400).json({ message: 'Invalid data format' });
-          } else if (e instanceof BaseError) {
-            res.status(400).json({ message: e.message });
-          } else {
-            res.status(500);
-          }
+      if (isValidationError(e)) {
+        res.status(400).json({ message: e.message });
+      } else if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        res.status(500).json({ message: e.meta });
+      } else if (e instanceof Prisma.PrismaClientValidationError) {
+        res.status(400).json({ message: 'Invalid data format' });
+      } else {
+        console.error(e);
+        res.status(500).json({ message: 'something went wrong' });
       }
     } finally {
       res.end();
