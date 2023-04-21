ef09682e9980cfc04cf06698a22b2cbbbb356d2d
Harvey Zhao <86583647+harveyzzhao@users.noreply.github.com>
2023-04-20 17:09:41 -0700

    Finish base job page (#16)
    * Wrapped title, lab name into the header banner
    
    TODO: place header banner into the job modal
          add the detail section containing job.description
    
    * Expanded prisma localization readme, changed look
    
    now Using wrapper to control the job posting's proportion to the screen
    width. Clarified variable names, and created new ES6 arrow functions as
    subcomponents of jobs.
    
    * Added tags section displaying departments, dur etc
    
    Not sure if the schema is correct. Will update on the next run.
    
    * SELECT dep, dur, car in postgres and display tags
    
    * Add margin between block-level elements in wrapper
    
    * Calc post age, added apply button, responsivify
    
    * Emergency patch: resolves time-related comp issues
    
    * Emergency Patch II: added luxon dep for DateTime
    
    Joseph, let me know if you want to end up using luxon.
    If approved, please run `yarn install` to update your dependencies.
    
    * fixed prisma manual and deleted package-lock
    
    * got rid of luxon
    
    * changed queries for jobid.jsx
    
    * changed jobid queries
    
    * Remove picture
    
    * Remove some checks
    
    * Revert query filter
    
    * Fix example .env var
    
    * Revert yarn.lock
    
    * Fix yarn.lock
    
    * Fix job query
    
    ---------
    
    Co-authored-by: Joseph Yu <39754176+joseph082@users.noreply.github.com>

--- pages/job/[jobId].jsx
+++ pages/job/[jobId].jsx
@@ -1,35 +1,156 @@
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
 
 function Job(props) {
-  // TODO: Display Job title, description, created Date, Lab name
   return (
-    <ul>
-      <li>{props.job.title}</li>
-      <li></li>
-    </ul>
+    <div className="max-w-full min-h-screen justify-items-center text-center mx-auto bg-blue-500 shadow-md flex flex-col items-center space-x-4">
+      <JobWrapper job={props.job} />
+      <ActionMenu />
+    </div>
   );
 }
 
+const JobWrapper = (props) => {
+  const { job } = props;
+  const { departments, duration, careerGoals } = job;
+
+  return (
+    <div className="w-10/12 pb-8 min-h-screen bg-blue-500">
+      <JobHeading job={job} />
+      <JobTagNav>
+        {departments.length > 0 ? (
+          departments.map((department) => <JobTagItem key={department}>{department}</JobTagItem>)
+        ) : (
+          <JobTagNav>No departments found</JobTagNav>
+        )}
+        {<JobTagItem>{duration}</JobTagItem>}
+        {careerGoals.map((careerGoal) => (
+          <JobTagItem key={careerGoal}>{careerGoal}</JobTagItem>
+        ))}
+      </JobTagNav>
+
+      <JobHero job={job} />
+    </div>
+  );
+};
+
+const JobHeading = (props) => {
+  const { job } = props;
+
+  return (
+    <div className="p-6 w-full mx-auto bg-white border border-gray-400 shadow-md flex text-left items-center space-x-4">
+      <div className="shrink-0">
+        <img
+          className="h-16 w-16 mx-1 bg-black"
+          src="https://www.w3schools.com/images/lamp.jpg"
+          alt="img"
+        />
+      </div>
+      <div>
+        <div className="text-base font-medium text-black my-0.5">{job.title}</div>
+      </div>
+    </div>
+  );
+};
+
+const JobTagNav = ({ children }) => {
+  return (
+    <div className="w-11/12 my-3 mx-auto px-3 py-2 bg-white rounded-lg text-black">
+      <nav className="py-4 px-6 text-sm font-medium">
+        <ul className="flex flex-row flex-wrap space-x-3">{children}</ul>
+      </nav>
+    </div>
+  );
+};
+
+// TODO: when jobs sorted by group is setup, add href to each tag
+const JobTagItem = ({ href, children = null }) => {
+  if (!children) {
+    return null;
+  }
+  return (
+    <li>
+      <a
+        href={href}
+        className={`block px-3 py-2 my-1 my rounded-md bg-violet-500 hover:bg-violet-600`}
+      >
+        {children}
+      </a>
+    </li>
+  );
+};
+
+const JobHero = (props) => {
+  const { job } = props;
+  return (
+    <div className="w-11/12 mx-auto p-6 w-full min-h-max bg-white rounded-lg shadow-md flex text-left text-black items-center space-x-4">
+      <JobDescription description={job.description} />
+    </div>
+  );
+};
+
+const JobDescription = (props) => {
+  const { description } = props;
+  return (
+    <div className="">
+      <p className="text-md font-medium text-slate-500 mb-1.5">Description</p>
+      <p className="text-sm font-light leading-normal">{description}</p>
+    </div>
+  );
+};
+
+const ActionMenu = ({ href }) => {
+  return (
+    <div className="fixed w-5/12 bottom-0 z-50">
+      <div className="bg-white border border-gray-300 rounded-lg shadow-lg">
+        <div className="p-4">
+          <div className="flex justify-center space-x-4">
+            <button className="w-3/12 bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold px-4 py-2 rounded mr-2">
+              Save
+            </button>
+            <button className="w-3/12 bg-blue-500 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded">
+              Apply
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export async function getStaticProps(context) {
-  // TODO: change prisma query using findUnique and context. Only select needed columns
-  const job = await prisma.job.findFirst({ include: { posters: true, lab: true } });
+  const job = await prisma.job.findUnique({
+    select: {
+      created: true,
+      id: true,
+      title: true,
+      description: true,
+      departments: true,
+      duration: true,
+      careerGoals: true,
+      closingDate: true,
+    },
+    where: { id: parseInt(context.params.jobId, 10) },
+  });
   // console.log({ job });
   return {
     props: {
       job: {
         ...job,
         created: JSON.parse(JSON.stringify(job.created)),
-        closingDate: JSON.parse(JSON.stringify(job.closingDate)),
+        ...(job.closingDate && { closingDate: JSON.parse(JSON.stringify(job.closingDate)) }),
       },
     },
   };
 }
 
 export async function getStaticPaths() {
-  // TODO: change prisma query. Only select needed column(s)
-  const jobs = await prisma.job.findMany();
+  // returns array containing jobId
+  const jobs = await prisma.job.findMany({
+    select: {
+      id: true,
+    },
+  });
 
   const paths = jobs.map((job) => ({
     params: { jobId: job.id.toString(10) },
