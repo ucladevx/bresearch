69c1644853736c983763a616d47cea575e89b744
Brenna Kj <116136318+brennakj3@users.noreply.github.com>
2023-05-21 17:05:18 -0700

    Moved colors inside extend in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 //Note about editing config:
 //Code in the root of theme will override the Tailwind default (if there is one)
-//If you want to add a new styling without replacing an old one, put it in extend
 //might need custom font size for 32 px
 module.exports = {
   content: ['./pages/**/*.jsx', './components/**/*.jsx'],
   theme: {
-    colors: {
-      'dark-blue': '#1E2F97',
-      'light-blue': '#85BDE5',
-      'light-green': '#B5E585',
-      'header-black': '#242429',
-      'text-black': '#404040',
-      'light-gray': '#F5F5F5',
+    extend: {
+      colors: {
+        'dark-blue': '#1E2F97',
+        'light-blue': '#85BDE5',
+        'light-green': '#B5E585',
+        'header-black': '#242429',
+        'text-black': '#404040',
+        'light-gray': '#F5F5F5',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
