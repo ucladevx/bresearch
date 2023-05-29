e8de93c680b2e82f63d5cca876756e3af8a61c9f
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2023-05-29 13:07:07 -0700

    Update seeding script
    Should've been in #40

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,6 +3,49 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
+  const userA = await prisma.user.upsert({
+    where: { email: 'johndoe@g.ucla.edu' },
+    create: {
+      email: 'johndoe@g.ucla.edu',
+    },
+    update: {},
+  });
+  const researcherA = await prisma.researcher.upsert({
+    where: { email: userA.email },
+    create: {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: userA.email,
+      user: {
+        connect: { email: userA.email },
+      },
+    },
+    update: {},
+  });
+  const userB = await prisma.user.upsert({
+    where: { email: 'janedoe@g.ucla.edu' },
+    create: {
+      email: 'janedoe@g.ucla.edu',
+    },
+    update: {},
+  });
+
+  const studentB = await prisma.student.upsert({
+    where: {
+      email: userB.email, // or your email here for debug purposes
+    },
+    create: {
+      firstName: 'Jane',
+      preferredName: 'Jane',
+      lastName: 'Doe',
+      email: userB.email,
+      user: {
+        connect: { email: userB.email },
+      },
+    },
+    update: {},
+  });
+
   const job = await prisma.job.upsert({
     where: { id: 1 },
     create: {
@@ -15,29 +58,9 @@ async function main() {
       weeklyHours: 10,
       credit: true,
       location: 'ON_CAMPUS',
-    },
-    update: {},
-  });
-
-  const researcherA = await prisma.researcher.upsert({
-    where: { email: 'johndoe@g.ucla.edu' },
-    create: {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'johndoe@g.ucla.edu',
-    },
-    update: {},
-  });
-
-  const studentA = await prisma.student.upsert({
-    where: {
-      email: 'janedoe@g.ucla.edu', // or your email here for debug purposes
-    },
-    create: {
-      firstName: 'Jane',
-      preferredName: 'Jane',
-      lastName: 'Doe',
-      email: 'janedoe@g.ucla.edu',
+      posters: {
+        connect: [{ email: researcherA.email }],
+      },
     },
     update: {},
   });
@@ -46,12 +69,12 @@ async function main() {
     where: {
       jobId_applicantEmail: {
         jobId: job.id,
-        applicantEmail: studentA.email,
+        applicantEmail: studentB.email,
       },
     },
     create: {
       jobId: job.id,
-      applicantEmail: studentA.email,
+      applicantEmail: studentB.email,
       bookmarked: true,
       status: 'SAVED',
     },
