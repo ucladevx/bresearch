19a02cfa2ceb5aa1007df3b19dc521a7da191930
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2023-05-29 15:10:03 -0700

    Change class to className

--- pages/account-type.jsx
+++ pages/account-type.jsx
@@ -4,18 +4,18 @@ import Image from 'next/image';
 function accountType() {
   return (
     <div>
-      <img src="/bResearchLogo.png" class="w-1/4 mx-auto my-20" />
-      <button class=" ml-60 w-1/3 h-60 bg-blue-800 text-white text-2xl font-normal rounded-md cursor-pointer">
+      <img src="/bResearchLogo.png" className="w-1/4 mx-auto my-20" />
+      <button className=" ml-60 w-1/3 h-60 bg-blue-800 text-white text-2xl font-normal rounded-md cursor-pointer">
         I am a student looking for research
         <br />
         <br />
-        <span class="font-bold">Create a Student Account</span>
+        <span className="font-bold">Create a Student Account</span>
       </button>
-      <button class=" ml-10 w-1/3 h-60 bg-blue-800 text-white text-2xl font-normal rounded-md cursor-pointer">
+      <button className=" ml-10 w-1/3 h-60 bg-blue-800 text-white text-2xl font-normal rounded-md cursor-pointer">
         I am a PI looking to hire students
         <br />
         <br />
-        <span class="font-bold">Create a PI Account</span>
+        <span className="font-bold">Create a PI Account</span>
       </button>
     </div>
   );
