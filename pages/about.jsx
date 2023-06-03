f93e498ec36427d15e7b1d97758db00ebedb0c69
Rohan Gandhi <rohangandhi202@g.ucla.edu>
2023-06-03 15:47:00 -0700

    implemented grid into the landing page

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,91 +10,82 @@ function LandingPage() {
 
       {/* First Section */}
       <main>
-        <section>
-          <div className="flex items-center">
-            <div className="flex-1 p-10 pr-10 pb-30 pl-10 mt-10 mr-20 mb-30 ml-20">
-              <h1 className="mt-4 mb-4 text-left text-black text-xl">
-                Countless Research Opportunities,
-                <br />
-                All In One Place
-              </h1>
-              <p className="mt-4 mb-4 text-left">
-                bResearch is a new web app that connects ucla
-                <br />
-                students to research opportunities, completely free for
-                <br />
-                everyone.
-              </p>
-              <button className="mt-24 ml-28 w-28 bg-blue-500 text-white py-4 px-0 text-lg font-medium rounded-md cursor-pointer">
-                Sign Up
-              </button>
-            </div>
-            <div className="flex-1">
-              <img
-                src="/BaseImage.png"
-                alt="Generic Picture"
-                width={748}
-                height={641}
-                class="float-right clear-both pt-20 ml-20"
-              />
-            </div>
+        <section className="grid grid-cols-2 gap-10 items-center">
+          <div className="p-10">
+            <h1 className="mt-4 mb-4 text-black text-xl">
+              Countless Research Opportunities,
+              <br />
+              All In One Place
+            </h1>
+            <p className="mt-4 mb-4">
+              bResearch is a new web app that connects UCLA
+              <br />
+              students to research opportunities, completely free for
+              <br />
+              everyone.
+            </p>
+            <button className="mt-24 w-28 bg-blue-500 text-white py-4 px-0 text-lg font-medium rounded-md cursor-pointer">
+              Sign Up
+            </button>
+          </div>
+          <div>
+            <img
+              src="/BaseImage.png"
+              alt="Generic Picture"
+              width={748}
+              height={641}
+              className="float-right clear-both pt-20"
+            />
           </div>
         </section>
 
         {/* Second Section */}
-        <section>
-          <div className="flex items-center">
-            <div className="flex-1">
-              <img
-                src="/BaseImage.png"
-                alt="Generic Picture"
-                width={748}
-                height={641}
-                class="float-right clear-both pl-20 pt-20 ml-20"
-              />
-            </div>
-            <div className="flex-1 p-10 pr-5 pb-30 pl-80 mt-10 mr-10 mb-30 ml-10">
-              <h1 className="mt-4 mb-4 text-left text-black text-xl">
-                Tired Of Cold Emailing Professors?
-              </h1>
-              <p className="mt-4 mb-4 text-left">
-                bResearch connects your detailed application to every
-                <br />
-                professor-- no more lost emails in the pile.
-              </p>
-            </div>
+        <section className="grid grid-cols-2 gap-10 items-center">
+          <div>
+            <img
+              src="/BaseImage.png"
+              alt="Generic Picture"
+              width={748}
+              height={641}
+              className="float-right clear-both pl-20 pt-20"
+            />
+          </div>
+          <div className="p-10">
+            <h1 className="mt-4 mb-4 text-black text-xl">Tired Of Cold Emailing Professors?</h1>
+            <p className="mt-4 mb-4">
+              bResearch connects your detailed application to every
+              <br />
+              professor-- no more lost emails in the pile.
+            </p>
           </div>
         </section>
 
         {/* Third Section */}
-        <section>
-          <div className="flex items-center">
-            <div className="flex-1 p-10 pr-10 pb-30 pl-10 mt-10 mr-20 mb-30 ml-20">
-              <h1 className="mt-4 mb-4 text-left text-black text-xl">
-                Stay Organized In Your Search
-                <br />
-                For Your Next Research Opportunity.{' '}
-              </h1>
-              <p className="mt-4 mb-4 text-left">
-                {' '}
-                We have developed features such as bookmarking,
-                <br />
-                application tracking, and a refined filter to help you
-                <br />
-                optimize your search for your next research
-                <br />
-                opportunity.
-              </p>
-            </div>
-            <div className="flex-1">
-              <img
-                src="/BaseImage.png"
-                alt="Generic Picture"
-                width={748}
-                height={641}
-                class="float-right clear-both pt-20 ml-20"
-              />
-            </div>
+        <section className="grid grid-cols-2 gap-10 items-center">
+          <div className="p-10">
+            <h1 className="mt-4 mb-4 text-black text-xl">
+              Stay Organized In Your Search
+              <br />
+              For Your Next Research Opportunity.{' '}
+            </h1>
+            <p className="mt-4 mb-4">
+              We have developed features such as bookmarking,
+              <br />
+              application tracking, and a refined filter to help you
+              <br />
+              optimize your search for your next research
+              <br />
+              opportunity.
+            </p>
+          </div>
+          <div>
+            <img
+              src="/BaseImage.png"
+              alt="Generic Picture"
+              width={748}
+              height={641}
+              className="float-right clear-both pt-20"
+            />
           </div>
         </section>
 
