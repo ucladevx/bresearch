b8c86140e3064a3a3613ccb32b2c416a204357f1
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2023-07-03 21:57:51 -0700

    Update JWT callback to work with middleware

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import prisma from '@lib/prisma';
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -31,38 +32,43 @@ export const authOptions = {
     async jwt({ token, account, profile }) {
       if (account) {
         // only truthy during signin https://next-auth.js.org/configuration/callbacks#jwt-callback
-        const currentUser = await prisma.user.findUnique({
+        const currentStudent = await prisma.student.findUnique({
           where: { email: profile.email },
-          select: {
-            student: true,
-            researcher: true,
-          },
         });
-        if (currentUser.student || currentUser.studentId) {
+        if (currentStudent) {
           token.accountType = 'student';
-        } else if (currentUser.researcher || currentUser.researcherId) {
-          token.accountType = 'researcher';
         } else {
+          const currentResearcher = await prisma.researcher.findUnique({
+            where: { email: profile.email },
+          });
+          if (currentResearcher) {
+            token.accountType = 'researcher';
+          } else {
+            // console.log('signin unselected');
+            token.accountType = null;
+          }
         }
-        console.log({ currentUser, accountType: token.accountType }, 'jwt');
-      } else if (token && !token.accountType) {
-        const currentUser = await prisma.user.findUnique({
-          where: { email: token.email },
-          select: {
-            student: true,
-            researcher: true,
-          },
-        });
-
-        if (currentUser.student || currentUser.studentId) {
-          token.accountType = 'student';
-        } else if (currentUser.researcher || currentUser.researcherId) {
-          token.accountType = 'researcher';
+      } else if (token) {
+        if (!token.accountType) {
+          const currentStudent = await prisma.student.findUnique({
+            where: { email: token.email },
+          });
+          if (currentStudent) {
+            token.accountType = 'student';
+          } else {
+            const currentResearcher = await prisma.researcher.findUnique({
+              where: { email: token.email },
+            });
+            if (currentResearcher) {
+              token.accountType = 'researcher';
+            } else {
+              token.accountType = null;
+              // console.log('not signin unselected');
+            }
+          }
         }
-
-        console.log({ currentUser, accountType: token.accountType }, 'jwt');
       }
-      console.log({ account, profile }, 'jwt');
+      // console.log({ account, profile, token }, 'jwt');
       return token;
     },
   },
