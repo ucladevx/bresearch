39713d691efb3f414603907057d34e3cf21bdd4b
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2023-07-20 23:51:47 -0700

    Fix main Menu.Button not closing menu when clicked
    Change other Menu.Buttons to buttons

--- components/TagDropdown.jsx
+++ components/TagDropdown.jsx
@@ -25,7 +25,7 @@ export default function TagDropdown() {
     <Menu as="div" className="relative inline-block">
       <Menu.Button
         //Changing button color based on the current selection
-        className={` ${textColor} ${bgColor} ${buttonStyle} `}
+        className={`${textColor} ${bgColor} ${buttonStyle}`}
       >
         {tag}
         <ChevronDownIcon className={`-mr-1 -ml-1 h-5 w-5 ${textColor}`} aria-hidden="true" />
@@ -45,9 +45,10 @@ export default function TagDropdown() {
         <Menu.Items className="fixed z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1 ">
             {/*Considering */}
-            <Menu.Item className="block px-4 py-2 text-sm">
+            <Menu.Item className="block px-4 py-2 text-sm" as="div">
               {({ active }) => (
-                <a
+                <button
+                  className={`text-[#1E2F97] bg-light-blue bg-opacity-30 hover:bg-light-blue hover:bg-opacity-50 ${buttonStyle}`}
                   onClick={() => {
                     //Setting color and title
                     setBgColor(
@@ -57,19 +58,16 @@ export default function TagDropdown() {
                     setTag('CONSIDERING');
                   }}
                 >
-                  <Menu.Button
-                    className={`text-[#1E2F97] bg-light-blue bg-opacity-30 hover:bg-light-blue hover:bg-opacity-50 ${buttonStyle}`}
-                  >
-                    CONSIDERING
-                  </Menu.Button>
-                </a>
+                  CONSIDERING
+                </button>
               )}
             </Menu.Item>
 
             {/*Accepted */}
-            <Menu.Item className="block px-4 py-2 text-sm">
+            <Menu.Item className="block px-4 py-2 text-sm" as="div">
               {({ active }) => (
-                <a
+                <button
+                  className={`text-[#29570D] bg-light-green bg-opacity-40 hover:bg-light-green hover:bg-opacity-60 ${buttonStyle}`}
                   onClick={() => {
                     setBgColor(
                       'bg-light-green bg-opacity-40 hover:bg-light-green hover:bg-opacity-60'
@@ -78,69 +76,56 @@ export default function TagDropdown() {
                     setTag('ACCEPTED');
                   }}
                 >
-                  <Menu.Button
-                    className={`text-[#29570D] bg-light-green bg-opacity-40 hover:bg-light-green hover:bg-opacity-60 ${buttonStyle}`}
-                  >
-                    ACCEPTED
-                  </Menu.Button>
-                </a>
+                  ACCEPTED
+                </button>
               )}
             </Menu.Item>
 
             {/*Interviewing */}
-            <Menu.Item className="block px-4 py-2 text-sm">
+            <Menu.Item className="block px-4 py-2 text-sm" as="div">
               {({ active }) => (
-                <a
+                <button
+                  className={`text-[#2A0062] bg-[#6f32be] bg-opacity-20 hover:bg-[#6f32be] hover:bg-opacity-30 ${buttonStyle}`}
                   onClick={() => {
                     setBgColor('bg-[#6f32be] bg-opacity-20 hover:bg-[#6f32be] hover:bg-opacity-30');
                     setTextColor('text-[#2A0062]');
                     setTag('INTERVIEWING');
                   }}
                 >
-                  <Menu.Button
-                    className={`text-[#2A0062] bg-[#6f32be] bg-opacity-20 hover:bg-[#6f32be] hover:bg-opacity-30 ${buttonStyle}`}
-                  >
-                    INTERVIEWING
-                  </Menu.Button>
-                </a>
+                  INTERVIEWING
+                </button>
               )}
             </Menu.Item>
 
             {/*Not Accepted*/}
-            <Menu.Item className="block px-4 py-2 text-sm">
+            <Menu.Item className="block px-4 py-2 text-sm" as="div">
               {({ active }) => (
-                <a
+                <button
+                  className={`text-[#570D0D] bg-[#E53939] bg-opacity-20 hover:bg-[#E53939] hover:bg-opacity-40 ${buttonStyle}`}
                   onClick={() => {
                     setBgColor('bg-[#E53939] bg-opacity-20 hover:bg-[#E53939] hover:bg-opacity-40');
                     setTextColor('text-[#570D0D]');
                     setTag('NOT ACCEPTED');
                   }}
                 >
-                  <Menu.Button
-                    className={`text-[#570D0D] bg-[#E53939] bg-opacity-20 hover:bg-[#E53939] hover:bg-opacity-40 ${buttonStyle}`}
-                  >
-                    NOT ACCEPTED
-                  </Menu.Button>
-                </a>
+                  NOT ACCEPTED
+                </button>
               )}
             </Menu.Item>
 
             {/*Joined */}
-            <Menu.Item className="block px-4 py-2 text-sm">
+            <Menu.Item className="block px-4 py-2 text-sm" as="div">
               {({ active }) => (
-                <a
+                <button
+                  className={`text-[#141466] bg-[#1E2F97] bg-opacity-20 hover:bg-[#1E2F97] hover:bg-opacity-30 ${buttonStyle}`}
                   onClick={() => {
                     setBgColor('bg-[#1E2F97] bg-opacity-20 hover:bg-[#1E2F97] hover:bg-opacity-30');
                     setTextColor('text-[#141466]');
                     setTag('JOINED');
                   }}
                 >
-                  <Menu.Button
-                    className={`text-[#141466] bg-[#1E2F97] bg-opacity-20 hover:bg-[#1E2F97] hover:bg-opacity-30 ${buttonStyle}`}
-                  >
-                    JOINED
-                  </Menu.Button>
-                </a>
+                  JOINED
+                </button>
               )}
             </Menu.Item>
           </div>
