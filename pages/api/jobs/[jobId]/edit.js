3b7c640d0635df19b36c6ff043a4a38feb26f26e
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2023-11-14 07:01:28 -0800

    Update job creation and editing page. Install @hapi/tlds. Add Durations to globals (#102)
    * Install @hapi/tlds
    
    * Change Job credit field to use a string instead of boolean
    
    * Add Durations to globals
    
    * Update JobCreationForm validator to validate all fields in form
    
    * Add duration, paid, and creditDescription fields. Allow email apps
    
    * Update JobCreation validator
    
    * Require weeklyHours to be nonnegative
    
    * Create job with creditDescription and correct applicationLink. Ensure user is in lab
    
    * Select each lab's contactEmail for JobCreationForm
    
    * Update EditJobRoute to use PUT and mimic job JobCreationRoute
    
    * Add logging for bad job descriptions. Clean up
    
    * Create new CloseJobRoute instead of being in edit route
    
    * Finish EditJob page functionality. Use new CloseJobRoute
    
    * Clean up
    
    * Remove unused validators

--- pages/api/jobs/[jobId]/edit.js
+++ pages/api/jobs/[jobId]/edit.js
@@ -1,7 +1,5 @@
 import { Prisma } from 'prisma/prisma-client';
-
-import { JobEditValidator, isValidationError } from '@lib/validators';
-// import { sanitize } from 'isomorphic-dompurify';
+import { JobCreationValidator, isValidationError } from '@lib/validators';
 import { JSDOM } from 'jsdom';
 import DOMPurify from 'dompurify';
 
@@ -17,7 +15,7 @@ class EditJobRoute extends ApiRoute {
    * @param {import('prisma/prisma-client').PrismaClient} prisma
    * @returns
    */
-  async patch(req, res, prisma) {
+  async put(req, res, prisma) {
     try {
       if (req.token.accountType !== 'researcher') {
         return res
@@ -25,23 +23,23 @@ class EditJobRoute extends ApiRoute {
           .json({ message: 'You are not a researcher', accountType: req.token.accountType });
       }
 
-      const { error, value } = JobEditValidator.validate(req.body);
+      const { error, value } = JobCreationValidator.validate(req.body);
 
       if (error) {
         throw error;
       }
-      if (req.body.description !== undefined) {
-        const sanitizedHTML = purify.sanitize(req.body.description, {
-          ALLOWED_TAGS: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'em', 'u', '#text'],
-          ALLOWED_ATTR: [],
-          KEEP_CONTENT: false,
+      const sanitizedHTML = purify.sanitize(req.body.description, {
+        ALLOWED_TAGS: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'em', 'u', '#text'],
+        ALLOWED_ATTR: [],
+        KEEP_CONTENT: false,
+      });
+      if (sanitizedHTML !== req.body.description) {
+        console.log(
+          `${req.session.user.email} tried creating a job with this invalid description:\n${req.body.description}`
+        );
+        return res.status(400).json({
+          message: 'Invalid job description',
         });
-        if (sanitizedHTML !== req.body.description) {
-          console.error({ sanitizedHTML, description: req.body.description });
-          return res.status(400).json({
-            message: 'Invalid job description',
-          });
-        }
       }
 
       // TODO: maybe use an interactive transaction https://www.prisma.io/docs/concepts/components/prisma-client/transactions#interactive-transactions
@@ -60,12 +58,78 @@ class EditJobRoute extends ApiRoute {
         return res.status(400).json({ message: '' });
       }
 
+      const {
+        startDate,
+        closingDate,
+        title,
+        description,
+        paid,
+        duration,
+        departments,
+        weeklyHours,
+        creditDescription,
+        location,
+        lab: labId,
+        applicationType,
+        externalLink,
+      } = value;
+
+      // TODO: check if researcher should have permission without this query
+      const lab = await prisma.lab.findUnique({
+        where: { id: labId },
+        select: { contactEmail: true, adminResearchers: { select: { email: true } } },
+      });
+      if (lab === null) {
+        return res.status(404).json({
+          message: 'Lab not found',
+        });
+      }
+      if (!lab.adminResearchers.some((r) => r.email === req.session.user.email)) {
+        return res.status(403).json({
+          message: "You aren't in this lab",
+        });
+      }
+
+      const closeDate = new Date(closingDate);
+
+      let applicationLink = null;
+      if (applicationType === 'email') {
+        applicationLink = lab.contactEmail;
+      } else if (applicationType === 'external') {
+        applicationLink = externalLink;
+      }
+
       const updatedJob = await prisma.job.update({
         where: {
           id: jobId,
         },
-        data: value.closed ? { closed: true, closingDate: new Date() } : value,
+        // data: value.closed ? { closed: true, closingDate: new Date() } : value,
+        data: {
+          closingDate: closeDate,
+          closed: closeDate ? closeDate < new Date() : false,
+          title,
+          location,
+          description,
+          // poster: {
+          //   connect: { email: req.session.user.email },
+          // },
+          // lab: {
+          //   connect: { id: labId },
+          // },
+          paid,
+          duration,
+          departments,
+          weeklyHours,
+          credit: creditDescription ?? null,
+          location,
+          externalLink: applicationLink,
+          startDate,
+        },
       });
+
+      await res.revalidate(`/job/${updatedJob.id}`);
+      await res.revalidate('/');
+
       res.status(200).json(updatedJob);
     } catch (e) {
       console.error({ e });
