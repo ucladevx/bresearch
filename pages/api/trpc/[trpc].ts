ab2c910928d1e4eb313ea819eb457ed1846512cc
Vikram <alexgiantseed@gmail.com>
2024-05-16 19:08:01 -0700

    changes

---
+++ pages/api/trpc/[trpc].ts
@@ -0,0 +1,46 @@
+import * as trpcNext from '@trpc/server/adapters/next';
+import { publicProcedure, router } from '../../../trpc.js'
+import { z } from 'zod';
+
+const appRouter = router({
+    jobs: publicProcedure
+        .input(
+            z.object({
+                studentid: z.string()
+            })
+        )
+        .query(({ input }) => {
+            let result = await prisma.job.findMany({
+                where: {
+                    closed: false,
+                    closingDate: { gt: new Date() },
+                    title: {
+                        contains: input,
+                        mode: 'insensitive',
+                    },
+                },
+                select: {
+                    id: true,
+                    title: true,
+                    description: true,
+                    departments: true,
+                    duration: true,
+                    location: true,
+                    lab: { select: { name: true } },
+                    created: true,
+                    startDate: true,
+                    closingDate: true,
+                    credit: true,
+                    weeklyHours: true,
+                    paid: true,
+                    externalLink: true,
+                    _count: {
+                        select: {
+                            applicants: { where: { status: 'APPLIED' } },
+                        },
+                    },
+                },
+                take: 50,
+            });
+        })
+})
